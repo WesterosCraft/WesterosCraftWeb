e2f19877071aea760525e7d1cf236ce2026c9688
jacob.granberry <jacobgranberry@gmail.com>
2022-07-23 15:54:51 -0500

    adds some templats

--- components/Layout/OneColumnLayout.tsx
+++ components/Layout/OneColumnLayout.tsx
@@ -1,12 +1,12 @@
 import { Flex } from "@chakra-ui/react";
-import { Nav } from "./Nav";
 import { SocialFooter } from "../SocialFooter";
+import { Navbar } from "../Navbar";
 
 export const OneColumnLayout = ({ children }: any) => {
   return (
     <>
       <Flex direction='column' minHeight='100vh' className='one-column-layout'>
-        <Nav position='inherit' />
+        <Navbar />
         <Flex
           flex='1 1'
           width='full'
