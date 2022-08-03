e377806d8eb728b2856c2fbba3ca6885a567cad2
jacob.granberry <jacob.granberry@company-corp.com>
2022-08-02 22:01:48 -0500

    serve feature

--- components/ServerFeatureGrid.tsx
+++ components/ServerFeatureGrid.tsx
@@ -1,20 +1,28 @@
-import { Box, SimpleGrid, Img, Center, VStack } from '@chakra-ui/react';
+import { Box, SimpleGrid, Img, Center, VStack, Stack } from '@chakra-ui/react';
 import Longclaw from '../public/longclaw.png';
 
 export const ServerFeatureGrid = () => {
   return (
     <Box w="full" className="serverFeatureGrid">
-      <Box maxW="8xl" mx="auto" px={[2, null, 4]} className="container">
-        <SimpleGrid columns={2} gap={16}>
-          <Center mx="auto" maxW="7xl" p="4">
+      <Box position="relative" maxW="7xl" mx="auto" px={[2, null, 4]} className="container">
+        <Stack
+          direction={{ base: 'column', lg: 'row' }}
+          borderLeftWidth="1px"
+          borderRightWidth="1px"
+          borderColor="primaryGold"
+          spacing={12}
+          mx="auto"
+          w="full"
+        >
+          <Center ml={-24}>
             <Img
-              width={700}
+              minW={750}
               src={
                 'https://cdn.sanity.io/images/1as7cn02/production/a8ec46b4bc30aba0ab00e5bd889c369d215ef6e7-1920x1200.png'
               }
             />
           </Center>
-          <VStack>
+          <VStack w="full" spacing={12}>
             <Box
               w="full"
               height={200}
@@ -34,7 +42,7 @@ export const ServerFeatureGrid = () => {
               {/* <Img position="absolute" top={-10} left={4} width={85} src={Longclaw.src} /> */}
             </Box>
           </VStack>
-        </SimpleGrid>
+        </Stack>
         {/* <SimpleGrid w="full" columns={{ base: 1, lg: 2 }} gap={16}>
           <Box
             w="full"
@@ -59,8 +67,3 @@ export const ServerFeatureGrid = () => {
     </Box>
   );
 };
-
-{
-  /* <Box w="full" className="imageGridFeature">
-<Box maxW="7xl" mx="auto" px={[2, null, 4]} className="container"> */
-}
